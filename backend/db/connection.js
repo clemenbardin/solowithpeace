e78fd7089de66e79ff5513c343e945627194caf1
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2026-05-19 16:11:10 +0200

    update docker compose - Connection dbb

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/solowithpeace';
+  const uri = process.env.MONGO_URI;
   await mongoose.connect(uri);
   console.log('[MongoDB] Connexion établie');
 }
